390129398666d6037bf38ba0d34c37c061044885
ablossomhead <aimeecapps@hotmail.com>
2020-08-24 12:40:44 -0400

    added ErrorBoundry around CardList

--- robofriends/src/containers/App.js
+++ robofriends/src/containers/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import './App.css';
-import Scroll from '../components/Scroll'
+import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry';
 // import { render } from '@testing-library/react';
 
 
@@ -35,7 +36,9 @@ class App extends Component {
                     <h1 className='f1'>RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange}/>
                     <Scroll>
-                        <CardList robots={filteredRobots} />
+                        <ErrorBoundry>
+                            <CardList robots={filteredRobots} />
+                        </ErrorBoundry>
                     </Scroll>
                 </div>
             );
